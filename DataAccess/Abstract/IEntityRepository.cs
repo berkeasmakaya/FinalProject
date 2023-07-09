21c61057cf244b6234b72babb57a7428128e6c35
Berke <asmakayab@gmail.com>
2023-07-10 01:05:19 +0300

    IEntityRepository, ICategoryDal, ICustomerDal was added.  Entityframework was installed to project

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+	//generic constraint
+	//class: refers to referance type
+	//IEntitiy: T can be IEntity or classes that is implemented by IEntity
+	//new: T can not be interface but can be class 
+	public interface IEntityRepository<T> where T : class,IEntity,new()
+	{
+		List<T> GetAll(Expression<Func<T, bool>> filter = null);
+		T Get(Expression<Func<T, bool>> filter);
+		void Add(T entity);
+		void Delete(T entity);
+		void Update(T entity);
+	}
+}
